7902e83b39d2b941e4e8f9b01514cd98ca410815
Hassan Raza <167288072+HassanRaza2020@users.noreply.github.com>
2025-01-12 23:23:44 +0500

    code updated

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -1,35 +1,34 @@
 <?php
 
 namespace App\Repositories;
+
+use App\Jobs\MailVerification;
 use App\Models\User;
 use App\Models\Verifications;
-use App\Jobs\MailVerification;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
+
 class BaseRepository
-
-
-{    
+{
     protected $verificationRepo, $mailVerification, $user;
-    
-    public function __construct(Verifications $verificationRepo , MailVerification $mailVerification, User $user )  //adding the constructor to add model
+
+    public function __construct(Verifications $verificationRepo, MailVerification $mailVerification, User $user) //adding the constructor to add model
     {
         $this->verificationRepo = $verificationRepo;
         $this->mailVerification = $mailVerification;
         $this->user = $user;
-        
-    }  
-    public function create($data,$otp)
-    {   
-        
-        $userInfo = [
-            'username' => $data->username,
-            'email' => $data->email,
-            'password' => $data->password,
-            'address' => $data->address
-        ];
-    
+
+    }
+    public function create($data, $otp)  //to create verification
+    {
         try {
+            $userInfo =
+                [
+                'username' => $data->username,
+                'email' => $data->email,
+                'password' => $data->password,
+                'address' => $data->address,
+            ];
             // Insert verification details into the database
             $this->verificationRepo::create([
                 'email' => $userInfo['email'], // The email should not be null
@@ -37,17 +36,16 @@ class BaseRepository
                 'expires_at' => Carbon::now()->addMinute(2), // OTP expiration time
             ]);
 
-
         } catch (\Exception $e) {
             // Log the exception message for debugging purposes
             Log::error('Verification creation failed: ' . $e->getMessage());
-        
+
             // Redirect back with an error message
             return redirect()->back()->with('error', 'Please enter the data from the form again.');
         }
 
-        return $userInfo;        
-    
+        return $userInfo;
+
     }
 
 }
