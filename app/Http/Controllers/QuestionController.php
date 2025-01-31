8ed0ddb22954b47b2d4c9b91578688cf8bbca275
Hassan Raza <167288072+HassanRaza2020@users.noreply.github.com>
2025-01-31 14:48:40 +0500

    file added

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -10,58 +10,45 @@ class QuestionController extends Controller
 
     protected $questionService;
 
-    public function __construct(QuestionService $questionService) //injecting the service class in the controller
+    public function __construct(QuestionService $questionService) 
     {
-        $this->questionService = $questionService; //copy constructor
-
+        $this->questionService = $questionService;
     }
 
     // Show categories for the ask-question form
     public function categoriesList()
     {
-        // Fetching all categories with 'content_id' and 'content_name'
-        $categories = $this->questionService->categoriesList();
-        // Pass categories to the view
-        return view('questions.askquestion', compact('categories'));
+       
+        $categories = $this->questionService->categoriesList();  // Fetching all categories with 'content_id' and 'content_name'
+        return view('questions.askquestion', compact('categories')); // Pass categories to the view
     }
 
     public function storeQuestion(QuestionRequest $request)
     {
         $this->questionService->create($request);
         return redirect()->route('questions')->with('success', 'Question submitted successfully!');
-
     }
 
     public function show()
     {
-
-        $questions = $this->questionService->getAllQuestion(); //using the show method from  service class
-
+        $questions = $this->questionService->getAllQuestion();    //using the show method from  service class
         return view('questions.questions', compact('questions')); //returning the view with question query
-
     }
 
     public function searchQuestion(Request $request)
     {
-
         $questions = $this->questionService->search($request->query('query')); //using the search method from service class
-
         return view('questions.questions', compact('questions'));
-
     }
 
     public function deleteQuestion($id)
     {
-
-        $this->questionService->delete($id); // Return the view with the updated questions
-        return response()->noContent();
+        return $this->questionService->delete($id); // Return the view with the updated questions
     }
 
     public function editQuestion(QuestionRequest $request)
     {
-        $data = $request->only(['title', 'description']);
-        $this->questionService->update($request->question_id, $data);
-        return redirect()->back()->with('status', 'Question updated successfully');
+        return $this->questionService->edit($request);
 
     }
 
